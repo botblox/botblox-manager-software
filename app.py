c21dd0cd3db10bf7e47ad0e395aaab9c4e2cf224
Aaron Elijah <aaronzakelijah@googlemail.com>
2020-12-16 12:31:11 +0000

    feat: new linting packages added

---
+++ app.py
@@ -0,0 +1,8 @@
+"""Module adding CLI subparsers together."""
+
+import datetime
+
+
+def test() -> datetime.datetime:
+    """The test function's docstring."""
+    return datetime.datetime.utcnow()
