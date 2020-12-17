6e51635a5ba775d0148a01a57eae9a7acee15f8c
AaronElijah <30408192+AaronElijah@users.noreply.github.com>
2020-12-17 12:22:19 +0000

    New test for sending pyserial commands from running docker container (#7)

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM continuumio/miniconda3:4.9.2
 COPY /app.py /botblox/
 COPY /requirements.txt /botblox/
 
-RUN conda create --name switchblox-env --file requirements.txt
-RUN echo "source activate env" > ~/.bashrc
+#RUN conda create --name switchblox-env --file /botblox/requirements.txt
+#RUN echo "source activate env" > ~/.bashrc
+RUN pip install -r /botblox/requirements.txt
 
 ENV PATH /opt/conda/envs/env/bin:$PATH
--- app.py
+++ app.py
@@ -3,9 +3,28 @@
 import datetime
 import logging
 
+import serial
+from serial.tools import list_ports
+
 logging.basicConfig(level=logging.DEBUG)
 
 
-def test() -> datetime.datetime:
-    """The test function's docstring."""
-    return datetime.datetime.utcnow()
+def test_pyserial(
+    #device_name: str,
+) -> None:
+    """Test serial commands can be sent given device."""
+    ser = serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate=115200,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        timeout=1000,
+        write_timeout=1000,
+    )
+    data = 45
+    logging.debug(list_ports.comports()[0])
+    ser.write(bytes(chr(data), encoding='utf8'))
+    ser.close()
+
+
+test_pyserial()
