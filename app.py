f98ddf819ee93f4e839f7ac28286b2b270c742ce
Aaron Elijah <aaronzakelijah@googlemail.com>
2020-12-26 18:53:19 +0000

    chore: cleanup code to make it more readable

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
 """File for defining BotBlox CLI."""
 
 import argparse
-import time
 import logging
 import sys
+import time
 from typing import (
     List,
     Optional,
 )
 
 import serial
-from serial.tools import list_ports
 
 from vlan_config import vlan_create_configuration
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 def write_data_to_serial(
     data: List[List],
     device_name: Optional[str] = '/dev/ttyUSB0',
 ) -> bool:
+    """Write data commands to serial port.
+
+    Write data to serial port which the UART converter is connected to. The STM32 MCU
+    on the SwitchBlox will then be interrupted and carry out the commands
+
+    :param List[List] data: Commands created to write to STM32 MCU
+    :param Optional[str] device_name: serial port device name (default: '/dev/ttyUSB0')
+    :return: Flag to indicate whether the write data to serial was successful or not
+    :rtype: bool
+    """
     ser = serial.Serial(
         port=device_name,
         baudrate=115200,
@@ -39,7 +49,7 @@ def write_data_to_serial(
 
     try:
         condition = list(condition)[0]
-    except IndexError as index_err:
+    except IndexError:
         logging.error('Failed to read condition message from board')
         return False
     else:
@@ -49,10 +59,10 @@ def write_data_to_serial(
         elif condition == 2:
             logging.error('Failed saving configuration in EEPROM')
             return False
-    
+
 
 def main_cli() -> None:
-    """Define all cli parser and subparsers here"""
+    """Define all cli parser and subparsers here."""
     parser = argparse.ArgumentParser(
         description='CLI for configuring SwitchBlox managed settings',
         epilog='Please open an issue on https://github.com/botblox/botblox-manager-software/ if there is a problem',
@@ -93,44 +103,6 @@ def main_cli() -> None:
         logging.info('Successful configuration')
     else:
         logging.error('Failed to configure - check logs')
-
-
-def test_pyserial(
-    # device_name: str,
-) -> None:
-    """Test serial commands can be sent given device."""
-    ser = serial.Serial(
-        port='/dev/ttyUSB0',
-        baudrate=115200,
-        bytesize=serial.EIGHTBITS,
-        parity=serial.PARITY_NONE,
-        timeout=20,
-        write_timeout=2,
-    )
-    # data = [[2, 0, 0, 0], [3, 0, 0, 0], [4, 0, 0, 48], [6, 0, 0, 48], [7, 0, 0, 0], [100, 0, 0, 0]]
-    data = [[101, 0, 0, 0], [100, 0, 0, 0]]
-
-    for command in data:
-        x = bytes(command)
-        ser.write(x)
-        time.sleep(0.1)
-
-    condition = ser.read(size=1)
-
-    try:
-        condition = list(condition)[0]
-    except IndexError as index_err:
-        logging.error('Failed to read condition message from board')
-    else:
-        if condition == 1:
-            logging.info('Success setting configuration in EEPROM')
-        elif condition == 2:
-            logging.error('Failed saving configuration in EEPROM')
-
-    ser.close()
-
-
-# test_pyserial()
 
 
 if __name__ == '__main__':
--- vlan_config.py
+++ vlan_config.py
@@ -1,5 +1,6 @@
+"""Define helper functions to convert the user vlan configuration to commands for the firmware."""
+
 from typing import (
-    Dict,
     List,
     Tuple,
     Union,
@@ -53,10 +54,32 @@ vlan_miim_register_map = {
     },
 }
 
+vlan_binary_offset_map = {
+        1: 2,
+        2: 3,
+        3: 4,
+        4: 6,
+        5: 7,
+}
+
 
 def get_unique_ports(
     numbers: List,
 ) -> List:
+    """Get unique member ports.
+
+    Returns only the unique ports that a single port shares VLAN membership with
+
+    :param List numbers: List of ports a single port shares VLAN membership, duplicates allowed.
+    :return: List of ports a single port shares VLAN membership, duplicates forbidden.
+    :rtype: List
+
+    :example:
+
+    >>> numbers=[1, 1, 2, 3, 4]
+    >>> get_unique_ports(numbers)
+    [1, 2, 3, 4]
+    """
     unique = []
     for number in numbers:
         if number not in unique:
@@ -64,23 +87,47 @@ def get_unique_ports(
     return unique
 
 
-def get_member_ports_per_port(
-    ports: List,
-    groupings: List[List],
-    vlan_members: Dict[int, List]
-) -> Dict[int, List]:
-    for port in ports:
-        for grouping in groupings:
-            if port in grouping:
-                vlan_members[port] += grouping
-            vlan_members[port] = get_unique_ports(vlan_members[port])
-    return vlan_members
+def create_vlan_parser_data_per_port(
+    vlan_members: List,
+    port: int,
+) -> None:
+    """Create section of parser data command per port.
+
+    Loops over members ports for each physical port and create the data in the shape so that it can be used
+    by the parser when further checks on duplicate commands are made.
+
+    :param List vlan_members: List of the member ports belonging to the physical port
+    :param int port: individual physical port
+    :rtype: None
+    """
+    for other_port in vlan_members:
+        vlan_miim_register_map[port]['data'] = vlan_miim_register_map[port]['data'] | (
+            1 << vlan_binary_offset_map[other_port])
 
 
 def is_command_already_present(
     data: List[Union[List, None]],
     port: int,
 ) -> Tuple[bool, Union[int, None]]:
+    """Calculate if a command writing to same PHY and REG exists.
+
+    Loops over current commands and determines if a command with the same PHY and REG is present.
+    If so, return a tuple with (True, <index_of_command_in_data>).
+    Else, return a tuple with (False, None)
+
+    :param List[Union[List, None]] data: Current commands created, nullable input.
+    :param int port: individual physical port
+    :return: object with boolean flag and index representing position in list where command already exists
+        for same PHY and REG
+    :rtype: Tuple[bool, Union[int, None]]
+
+    :example:
+
+    >>> numbers=[[23, 16, 0, 0]]
+    >>> port=2
+    >>> is_command_already_present(numbers, port)
+    (True, 0)
+    """
     if data:
         for index_in_data, command in enumerate(data):
             if vlan_miim_register_map[port]['phy'] == command[0] and vlan_miim_register_map[port]['reg'] == command[1]:
@@ -89,50 +136,88 @@ def is_command_already_present(
     return (False, None)
 
 
+def update_command(
+    command_to_update: List,
+    port: int,
+) -> List:
+    """Update a command when it is found that two parts of data share the same PHY and REG.
+
+    Use the `vlan_miim_register_map` to update the command with the register offset of the
+    new data that shared the same PHY and REG.
+
+    :param List command_to_update: Current command that needs to be updated with new data.
+    :param int port: individual physical port
+    :return: Updated command with data regarding the current port
+    :rtype: List
+    """
+    port_data = vlan_miim_register_map[port]['data']
+    port_offset = vlan_miim_register_map[port]['offset']
+
+    command_to_update[2] = command_to_update[2] | ((port_data << port_offset) & 0xFF)
+    command_to_update[3] = command_to_update[3] | (((port_data << port_offset) & 0xFF00) >> port_offset)
+
+    return command_to_update
+
+
+def create_command(
+    port: int,
+) -> List:
+    """Create a new command with PHY and REG plus two bytes of command data.
+
+    Use the `vlan_miim_register_map` to create the command
+
+    :param int port: individual physical port
+    :return: Newly created command
+    :rtype: List
+    """
+    port_data = vlan_miim_register_map[port]['data']
+    port_offset = vlan_miim_register_map[port]['offset']
+
+    command = [
+        vlan_miim_register_map[port]['phy'],
+        vlan_miim_register_map[port]['reg'],
+        (port_data << port_offset) & 0xFF,
+        ((port_data << port_offset) & 0xFF00) >> port_offset,
+    ]
+    return command
+
+
 def vlan_create_configuration(
-    groupings: List,
+    groupings: List[List],
 ) -> List[List]:
-    vlan_binary_offset_map = {
-        1: 2,
-        2: 3,
-        3: 4,
-        4: 6,
-        5: 7,
-    }
+    """Create parser data from user input.
 
-    vlan_members = {
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: [],
-    }
+    :param List[List] groupings: each list element is a VLAN group and sub-elements are the port members.
+    :return: list of commmands to be sent to the firmware
+    :rtype: List[List]
 
-    ports = list(vlan_members.keys())
-    vlan_members = get_member_ports_per_port(ports=ports, groupings=groupings, vlan_members=vlan_members)
+    :example:
 
-    for port in ports:
-        for other_port in vlan_members[port]:
-            vlan_miim_register_map[port]['data'] = vlan_miim_register_map[port]['data'] | (
-                1 << vlan_binary_offset_map[other_port])
-
+    >>> groupings=[[1, 2], [3, 4]]
+    >>> vlan_create_configuration(groupings)
+    [[23, 16, 12, 12], [23, 17, 80, 0], [23, 18, 80, 0]]
+    """
+    ports = list(vlan_binary_offset_map.keys())
     data = []
+
     for port in ports:
+        vlan_members = []
+        for grouping in groupings:
+            if port in grouping:
+                vlan_members += grouping
+
+        vlan_members = get_unique_ports(vlan_members)
+
+        create_vlan_parser_data_per_port(vlan_members=vlan_members, port=port)
+
         is_present, index_in_data = is_command_already_present(data, port)
+
         if is_present and index_in_data is not None:
             command_to_update = data[index_in_data]
-            command_to_update[2] = command_to_update[2] | ((vlan_miim_register_map[port]['data'] << vlan_miim_register_map[port]['offset']) & 0xFF)
-            command_to_update[3] = command_to_update[3] | (((vlan_miim_register_map[port]['data'] << vlan_miim_register_map[port]['offset']) & 0xFF00) >> vlan_miim_register_map[port]['offset'])
-            data[index_in_data] = command_to_update
+            command_updated = update_command(command_to_update=command_to_update, port=port)
+            data[index_in_data] = command_updated
         else:
-            command = []
-            if vlan_miim_register_map[port]['data'] != vlan_miim_register_map[port]['sys_default']:
-                command = [
-                    vlan_miim_register_map[port]['phy'],
-                    vlan_miim_register_map[port]['reg'],
-                    (vlan_miim_register_map[port]['data'] << vlan_miim_register_map[port]['offset']) & 0xFF,
-                    ((vlan_miim_register_map[port]['data'] << vlan_miim_register_map[port]['offset']) & 0xFF00) >> vlan_miim_register_map[port]['offset'],
-                ]
-                data.append(command)
+            command = create_command(port)
+            data.append(command)
 
     return data
